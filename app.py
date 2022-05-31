3acfd6f9d9475a4c8a6644af19b2927eb325f64a
Duda <eduardo_lh@protonmail.com>
2022-05-31 16:33:56 -0300

    Adding, custom response

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
-from flask import Flask
+from flask import Flask, json, Response
 from src.controller.service_consumer import get_info
 from config import app_config
+
+
+def custom_response(res, status_code):
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
 
 
 app = Flask(__name__)
 config = (app_config['development'])
 
+
 @app.get("/")
 def home():
-    return "Olá mundo!!"
+    return custom_response("Olá mundo!!", 200)
 
 
 @app.post("/int/<int:some_int>")
 def int_input(some_int):
-    return f"Numero {some_int}"
+    return custom_response(f"Numero {some_int}", 200)
 
 
 @app.route("/pega")
 async def GET():
     response = await get_info()
-    return response.json()[0:4]
+    return custom_response(response.json()[0:4], 200)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return f"error: {error}"
+    return custom_response(error, 404)
 
 
 if __name__ == '__main__':
     app.run(debug=config.DEBUG,
             host=config.HOST,
             port=config.PORT)
-
-
